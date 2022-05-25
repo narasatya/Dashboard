93afea842404165f8224be42b789ac9fc41f4c84
narasatya <narasatya1605@gmail.com>
2022-05-25 14:14:22 +0700

    tambah fitur dashboard

---
+++ Point_of_sale/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Point_of_sale.Startup))]
+namespace Point_of_sale
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
